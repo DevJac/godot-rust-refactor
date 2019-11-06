bd5c465a696c1b68c4bd48adb48792b4b264fdd7
Devin Jacobs <dbuttonz@gmail.com>
2019-11-05 22:15:22 -0700

    Ignore more warnings in bindgen code

--- refactor/gdnative-sys/src/lib.rs
+++ refactor/gdnative-sys/src/lib.rs
@@ -1,2 +1,8 @@
-#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
+#![allow(
+    non_upper_case_globals,
+    non_camel_case_types,
+    non_snake_case,
+    improper_ctypes,
+    clippy::style
+)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
