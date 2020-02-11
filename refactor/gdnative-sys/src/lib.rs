9605fc1805befee38cc21c362d0d5e4fbf55e413
Devin Jacobs <dbuttonz@gmail.com>
2020-02-11 11:31:28 -0700

    Fix some warnings (some from clippy)

--- refactor/gdnative-sys/src/lib.rs
+++ refactor/gdnative-sys/src/lib.rs
@@ -1,7 +1,8 @@
 #![allow(
-    non_upper_case_globals,
-    non_camel_case_types,
     improper_ctypes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
     clippy::style
 )]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
