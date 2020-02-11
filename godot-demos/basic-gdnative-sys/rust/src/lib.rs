62cec5afd7d6032649a30d6b4f3df5467cfef1af
Devin Jacobs <dbuttonz@gmail.com>
2020-02-11 11:48:36 -0700

    Fix basic-gdnative-sys demo

--- godot-demos/basic-gdnative-sys/rust/src/lib.rs
+++ godot-demos/basic-gdnative-sys/rust/src/lib.rs
@@ -24,16 +24,16 @@ pub unsafe extern "C" fn godot_gdnative_terminate(
 pub unsafe extern "C" fn add_42(args: *const sys::godot_array) -> sys::godot_variant {
     println!("FROM RUST: add_42 called");
     let api = API.as_ref().unwrap();
-    assert_eq!(api.godot_array_size(args), 1);
-    let godot_variant = api.godot_array_get(args, 0);
+    assert_eq!((api.godot_array_size)(args), 1);
+    let godot_variant = (api.godot_array_get)(args, 0);
     assert_eq!(
-        api.godot_variant_get_type(&godot_variant),
+        (api.godot_variant_get_type)(&godot_variant),
         sys::godot_variant_type_GODOT_VARIANT_TYPE_INT
     );
-    let n = api.godot_variant_as_int(&godot_variant);
+    let n = (api.godot_variant_as_int)(&godot_variant);
     let result = n + 42;
     let mut result_variant = MaybeUninit::<sys::godot_variant>::uninit();
-    api.godot_variant_new_int(result_variant.as_mut_ptr(), result);
+    (api.godot_variant_new_int)(result_variant.as_mut_ptr(), result);
     result_variant.assume_init()
 }
 
@@ -43,7 +43,7 @@ pub unsafe extern "C" fn godot_nativescript_init(handle: *mut c_void) {
     let api = API.as_ref().unwrap();
     let simple_cstr = CString::new("Simple").unwrap();
     let reference_cstr = CString::new("Reference").unwrap();
-    api.godot_nativescript_register_class(
+    (api.godot_nativescript_register_class)(
         handle,
         simple_cstr.as_ptr(),
         reference_cstr.as_ptr(),
@@ -59,7 +59,7 @@ pub unsafe extern "C" fn godot_nativescript_init(handle: *mut c_void) {
         },
     );
     let set_string_cstr = CString::new("set_string").unwrap();
-    api.godot_nativescript_register_method(
+    (api.godot_nativescript_register_method)(
         handle,
         simple_cstr.as_ptr(),
         set_string_cstr.as_ptr(),
@@ -73,7 +73,7 @@ pub unsafe extern "C" fn godot_nativescript_init(handle: *mut c_void) {
         },
     );
     let get_string_cstr = CString::new("get_string").unwrap();
-    api.godot_nativescript_register_method(
+    (api.godot_nativescript_register_method)(
         handle,
         simple_cstr.as_ptr(),
         get_string_cstr.as_ptr(),
@@ -95,7 +95,7 @@ pub unsafe extern "C" fn simple_constructor(
     println!("FROM RUST: constructor");
     let api = API.as_ref().unwrap();
     let rust_string = String::from("Default String");
-    let user_data = api.godot_alloc(size_of_val(&rust_string) as i32);
+    let user_data = (api.godot_alloc)(size_of_val(&rust_string) as i32);
     (user_data as *mut String).copy_from(&rust_string, 1);
     forget(rust_string);
     user_data
@@ -109,7 +109,7 @@ pub unsafe extern "C" fn simple_destructor(
     println!("FROM RUST: destructor");
     let api = API.as_ref().unwrap();
     ptr::drop_in_place(user_data as *mut String);
-    api.godot_free(user_data);
+    (api.godot_free)(user_data);
 }
 
 pub unsafe extern "C" fn set_string(
@@ -124,12 +124,12 @@ pub unsafe extern "C" fn set_string(
     assert_eq!(num_args, 1);
     let arg_1 = **args;
     assert_eq!(
-        api.godot_variant_get_type(&arg_1),
+        (api.godot_variant_get_type)(&arg_1),
         sys::godot_variant_type_GODOT_VARIANT_TYPE_STRING
     );
-    let godot_string = api.godot_variant_as_string(&arg_1);
-    let godot_char_string = api.godot_string_utf8(&godot_string);
-    let godot_string_data = api.godot_char_string_get_data(&godot_char_string);
+    let godot_string = (api.godot_variant_as_string)(&arg_1);
+    let godot_char_string = (api.godot_string_utf8)(&godot_string);
+    let godot_string_data = (api.godot_char_string_get_data)(&godot_char_string);
     let rust_string = CStr::from_ptr(godot_string_data)
         .to_str()
         .unwrap()
@@ -138,7 +138,7 @@ pub unsafe extern "C" fn set_string(
     (user_data as *mut String).copy_from(&rust_string, 1);
     forget(rust_string);
     let mut result_variant = MaybeUninit::<sys::godot_variant>::uninit();
-    api.godot_variant_new_nil(result_variant.as_mut_ptr());
+    (api.godot_variant_new_nil)(result_variant.as_mut_ptr());
     result_variant.assume_init()
 }
 
@@ -155,10 +155,10 @@ pub unsafe extern "C" fn get_string(
     let rust_string = (*(user_data as *const String)).as_str();
     let rust_cstring = CString::new(rust_string).unwrap();
     let mut godot_string = MaybeUninit::<sys::godot_string>::uninit();
-    api.godot_string_new(godot_string.as_mut_ptr());
-    api.godot_string_parse_utf8(godot_string.as_mut_ptr(), rust_cstring.as_ptr());
+    (api.godot_string_new)(godot_string.as_mut_ptr());
+    (api.godot_string_parse_utf8)(godot_string.as_mut_ptr(), rust_cstring.as_ptr());
     let mut result_variant = MaybeUninit::<sys::godot_variant>::uninit();
-    api.godot_variant_new_string(result_variant.as_mut_ptr(), godot_string.as_mut_ptr());
-    api.godot_string_destroy(godot_string.as_mut_ptr());
+    (api.godot_variant_new_string)(result_variant.as_mut_ptr(), godot_string.as_mut_ptr());
+    (api.godot_string_destroy)(godot_string.as_mut_ptr());
     result_variant.assume_init()
 }
